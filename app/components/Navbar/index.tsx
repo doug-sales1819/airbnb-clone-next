b016e99a3dad9eefbac19d29d489884d17a4d410
Douglas Sales <douglas.sales0611@gmail.com>
2023-05-20 02:21:11 -0300

    🧹 chore(inicialização do projeto): criada a estrutura inicial do projeto com next

---
+++ app/components/Navbar/index.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Navbar = () => {
+  return (
+    <nav className='fixed w-full bg-white z-10 shadow-sm'>
+      <div className='py-4 border-b=[1px]'>
+        teste
+      </div>
+    </nav>
+  )
+}
+
+export default Navbar

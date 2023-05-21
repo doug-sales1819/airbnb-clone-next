5c307660949a0f7b33d8407f3be31c2e395d7224
Douglas Sales <douglas.sales0611@gmail.com>
2023-05-21 17:03:39 -0300

    ✨ feat(navbar): componente Navbar parcialmente terminado
    falta finalizar o modal do usuário

--- app/components/Navbar/index.tsx
+++ app/components/Navbar/index.tsx
@@ -1,13 +1,33 @@
-import React from 'react'
+"use client";
+
+import Container from "../Container";
+import Logo from "./components/Logo";
+import Search from "./components/Search";
+import UserMenu from "./components/UserMenu";
 
 const Navbar = () => {
   return (
-    <nav className='fixed w-full bg-white z-10 shadow-sm'>
-      <div className='py-4 border-b=[1px]'>
-        teste
+    <nav className="fixed w-full bg-white z-10 shadow-sm">
+      <div className="py-4 border-b-[1px]">
+        <Container>
+          <div
+            className="
+            flex
+            flex-row
+            items-center
+            justify-between
+            gap-3
+            md:gap-0
+            "
+          >
+            <Logo />
+            <Search />
+            <UserMenu />
+          </div>
+        </Container>
       </div>
     </nav>
-  )
-}
+  );
+};
 
-export default Navbar
+export default Navbar;
--- app/page.tsx
+++ app/page.tsx
@@ -1,15 +1,11 @@
-import Image from 'next/image';
-
-import Navbar from './components/Navbar';
+import React from "react";
 
 const Home = () => {
   return (
     <main>
-      <Navbar />
-
-      <h1 className="text-rose-500 text-6xl">Hello Airbnb</h1>
+      <h1 className="text-rose-600 text-5xl">Airbnb page</h1>
     </main>
-  )
-}
+  );
+};
 
 export default Home;
